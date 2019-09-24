d66997b2497494dff79ce5c9ebcf89421dd3e658
Kyler Wong <wsm.kyler@gmail.com>
2019-09-24 15:09:49 +0800

    Added stub result tiles and screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,8 @@
 import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
-// import 'package:furniture_app/screens/details.dart';
-// import 'package:furniture_app/util/data.dart';
+import 'package:furniture_app/screens/details.dart';
+import 'package:furniture_app/util/data.dart';
 // import 'package:furniture_app/widgets/badge.dart';
 import 'package:provider/provider.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -132,6 +132,13 @@ class _HomeState extends State<Home> {
     );
     var buttons = [uploadMediaButton, liveButton];
 
+    var resultTilesBg = [
+      "assets/pic01.jpg",
+      "assets/pic02.jpg",
+      "assets/pic03.jpg",
+      "assets/pic04.jpg"
+    ];
+
     return Scaffold(
       appBar:
           PreferredSize(child: AppBar(), preferredSize: Size.fromHeight(0.0)),
@@ -193,117 +200,71 @@ class _HomeState extends State<Home> {
                     fontWeight: FontWeight.w800,
                   )),
             ),
-            // Container(
-            //   height: 275,
-            //   child: ListView.builder(
-            //     scrollDirection: Axis.horizontal,
-            //     shrinkWrap: true,
-            //     itemCount: furnitures.length,
-            //     itemBuilder: (BuildContext context, int index) {
-            //       Map furniture = furnitures[index];
-            //       return Padding(
-            //         padding: EdgeInsets.only(right: 20),
-            //         child: GestureDetector(
-            //           onTap: () {
-            //             Navigator.of(context).push(
-            //               MaterialPageRoute(
-            //                 builder: (BuildContext context) {
-            //                   return Details();
-            //                 },
-            //               ),
-            //             );
-            //           },
-            //           child: Container(
-            //             height: 275,
-            //             width: 280,
-            //             child: Column(
-            //               crossAxisAlignment: CrossAxisAlignment.start,
-            //               children: <Widget>[
-            //                 Text(
-            //                   furniture['name'],
-            //                   style: TextStyle(
-            //                     fontWeight: FontWeight.bold,
-            //                     fontSize: 20,
-            //                   ),
-            //                 ),
-            //                 SizedBox(height: 10),
-            //                 ClipRRect(
-            //                   borderRadius: BorderRadius.circular(15),
-            //                   child: Image.asset(
-            //                     "${furniture["img"]}",
-            //                     height: 240,
-            //                     width: 280,
-            //                     fit: BoxFit.cover,
-            //                   ),
-            //                 ),
-            //               ],
-            //             ),
-            //           ),
-            //         ),
-            //       );
-            //     },
-            //   ),
-            // ),
-            // SizedBox(height: 20),
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //   children: <Widget>[
-            //     Text(
-            //       "Popular Products",
-            //       style: TextStyle(
-            //         fontSize: 23,
-            //         fontWeight: FontWeight.w800,
-            //       ),
-            //     ),
-            //     FlatButton(
-            //       child: Text(
-            //         "View More",
-            //         style: TextStyle(
-            //           color: Colors.grey,
-            //         ),
-            //       ),
-            //       onPressed: () {},
-            //     ),
-            //   ],
-            // ),
-            // Container(
-            //   height: 140,
-            //   child: ListView.builder(
-            //     scrollDirection: Axis.horizontal,
-            //     shrinkWrap: true,
-            //     itemCount: furnitures.length,
-            //     itemBuilder: (BuildContext context, int index) {
-            //       Map furniture = furnitures[index];
-            //       return Padding(
-            //         padding: EdgeInsets.only(right: 20),
-            //         child: GestureDetector(
-            //           onTap: () {
-            //             Navigator.of(context).push(
-            //               MaterialPageRoute(
-            //                 builder: (BuildContext context) {
-            //                   return Details();
-            //                 },
-            //               ),
-            //             );
-            //           },
-            //           child: Container(
-            //             height: 140,
-            //             width: 140,
-            //             child: ClipRRect(
-            //               borderRadius: BorderRadius.circular(15),
-            //               child: Image.asset(
-            //                 "${furniture["img"]}",
-            //                 height: 140,
-            //                 width: 140,
-            //                 fit: BoxFit.cover,
-            //               ),
-            //             ),
-            //           ),
-            //         ),
-            //       );
-            //     },
-            //   ),
-            // ),
+            SizedBox(height: 20),
+            Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  physics: ClampingScrollPhysics(),
+                  itemCount: furnitures.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    // Map furniture = furnitures[index];
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (BuildContext context) {
+                                return Details();
+                              },
+                            ),
+                          );
+                        },
+                        child: Card(
+                          elevation: 6.0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(10.0),
+                              side: BorderSide(
+                                  color: isDark
+                                      ? const Color(0xff43464b)
+                                      : Colors.transparent)),
+                          child: Stack(
+                            children: <Widget>[
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.asset(
+                                  resultTilesBg[index % resultTilesBg.length],
+                                  // "${furniture["img"]}",
+                                  height: 140,
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.8,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Container(
+                                height: 140,
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text('Transcript ${(index + 1)}',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontFamily: "Helvetica",
+                                        fontWeight: FontWeight.w700,
+                                      )),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
             SizedBox(height: 10),
           ],
         ),
