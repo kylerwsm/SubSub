97fc3cb1b008558a1965780b1293a3549748df20
Kyler Wong <wsm.kyler@gmail.com>
2019-10-12 23:59:09 +0800

    Post Compilation Changes

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/kylerwong/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/kylerwong/Development/sub_sub"
-export "FLUTTER_TARGET=/Users/kylerwong/Development/sub_sub/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/kylerwong/Development/SubSub"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/kylerwong/Development/flutter/bin/cache/artifacts/engine/ios-release"
